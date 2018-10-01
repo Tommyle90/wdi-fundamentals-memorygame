2172edf7a0ec92fb0fa2ad3f03e22271278ce3fe
Tommy Le <tommy@Tommys-MacBook-Pro.local>
2018-10-01 14:05:28 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped" + '' + cardOne);
+console.log("user flipped" + '' + cardTwo);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+	alert("You Found a Match!");
+} 
+else {
+	alert("Sorry, Try Again.");
+}
+
+
+
